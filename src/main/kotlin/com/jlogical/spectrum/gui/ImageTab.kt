c1b8e6b6e57df1591d10d1d4ae1e0f61703e745a
Jake <jakeboychenko@yahoo.com>
2019-11-23 16:47:06 -0500

    Cleaned up ImageTab with more functions and comments.

--- src/main/kotlin/com/jlogical/spectrum/gui/ImageTab.kt
+++ src/main/kotlin/com/jlogical/spectrum/gui/ImageTab.kt
@@ -3,6 +3,7 @@ package com.jlogical.spectrum.gui
 import com.jlogical.spectrum.model.MunsellColor
 import javafx.geometry.Pos
 import javafx.scene.image.Image
+import javafx.scene.input.MouseEvent
 import javafx.scene.layout.BorderPane
 import javafx.scene.layout.HBox
 import javafx.scene.layout.VBox
@@ -32,31 +33,7 @@ class ImageTab : BorderPane() {
             button("Upload Image...") {
                 isDefaultButton = true
                 action {
-                    val file = chooseFile("Open Image", arrayOf(FileChooser.ExtensionFilter("Images (*.png, *.jpg)", "*.png", "*.jpg")), FileChooserMode.Single, null)
-                    if (file.isNotEmpty()) {
-                        image = Image(file[0].toURI().toString(), 450.0, 450.0, true, true)
-                        this@ImageTab.center = VBox(5.0).apply {
-                            alignment = Pos.CENTER
-
-                            this += PaletteBar()
-                            imageview(image!!).apply {
-                                setOnMousePressed {
-                                    this@ImageTab.bottom = HBox().apply {
-                                        alignment = Pos.CENTER
-                                        paddingAll = 4.0
-
-                                        this += MunsellColorBlock(MunsellColor.fromRGB(image.pixelReader.getColor(it.x.toInt(), it.y.toInt()))).apply {
-                                            setOnMousePressed { e ->
-                                                if (e.isPrimaryButtonDown && e.clickCount == 2)
-                                                    DetailsTab.showColorDetails(this.color)
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        buttonBar.isVisible = true
-                    }
+                    openImage()
                 }
             }
 
@@ -75,6 +52,44 @@ class ImageTab : BorderPane() {
                         this@ImageTab.bottom = null
                         buttonBar.isVisible = false
                     }
+                }
+            }
+        }
+    }
+
+    /**
+     * Opens an image and displays it.
+     */
+    private fun openImage(){
+        val file = chooseFile("Open Image", arrayOf(FileChooser.ExtensionFilter("Images (*.png, *.jpg)", "*.png", "*.jpg")), FileChooserMode.Single, null)
+        if (file.isNotEmpty()) {
+            image = Image(file[0].toURI().toString(), 450.0, 450.0, true, true)
+            center = VBox(5.0).apply {
+                alignment = Pos.CENTER
+
+                this += PaletteBar()
+                imageview(image!!).apply {
+                    setOnMousePressed {
+                        showMunsellBlock(it)
+                    }
+                }
+            }
+            buttonBar.isVisible = true
+        }
+    }
+
+    /**
+     * Displays a MunsellBlock on the bottom of the page with the clicked pixel.
+     */
+    private fun showMunsellBlock(me : MouseEvent){
+        bottom = HBox().apply {
+            alignment = Pos.CENTER
+            paddingAll = 4.0
+
+            this += MunsellColorBlock(MunsellColor.fromRGB(image!!.pixelReader.getColor(me.x.toInt(), me.y.toInt()))).apply {
+                setOnMousePressed { e ->
+                    if (e.isPrimaryButtonDown && e.clickCount == 2)
+                        DetailsTab.showColorDetails(this.color)
                 }
             }
         }
